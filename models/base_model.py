ef362af820a8ef0e857a37980de6c794e3e7c489
stepholo <oloobrian89@gmail.com>
2023-08-10 05:04:24 -0700

    Base Class

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            from models.user import User
             storage.new(self)
 
     def __str__(self):
