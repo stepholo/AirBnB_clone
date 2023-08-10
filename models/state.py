7fb1d188be0dbd31e6724dcfc08827dc5d713f65
stepholo <oloobrian89@gmail.com>
2023-08-10 08:51:52 -0700

    Child class State

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to define class State that inherits from class BaseModel"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State
+        Public Instance:
+            name: empty string
+    """
+    name = ""
