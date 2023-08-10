cec86faa22db0ecadca40912e774cf8df50eabae
stepholo <oloobrian89@gmail.com>
2023-08-10 08:51:10 -0700

    Child class Review

---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to define class"""
+
+
+from models.base_models import BaseModel
+
+
+class Review(BaseModel):
+    """Child class Review inherits form BaseModel
+        Public class attributes
+            place_id: empty string: it will be the Place.id
+            user_id: empty string: it will be the User.id
+            text: empty string
+    """
+    place_id = ""
+    user_id = ""
+    text: ""
